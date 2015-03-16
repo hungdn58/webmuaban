de6dbeede8cd90289150c59ac116797afbaf0072
Cao Minh Lam <caominhlam30395@gmail.com>
2015-03-16 23:06:20 +0700

    first commit
    tạo cơ sở dữ liệu

--- db/schema.rb
+++ db/schema.rb
@@ -11,22 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150226171502) do
+ActiveRecord::Schema.define(version: 20150203162941) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
-  create_table "conversations", force: :cascade do |t|
-    t.integer  "sender_id"
-    t.integer  "recipient_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "conversations", ["recipient_id"], name: "index_conversations_on_recipient_id"
-  add_index "conversations", ["sender_id"], name: "index_conversations_on_sender_id"
 
   create_table "line_items", force: :cascade do |t|
     t.integer  "product_id"
@@ -36,17 +26,6 @@ ActiveRecord::Schema.define(version: 20150226171502) do
     t.integer  "quantity",                           default: 1
     t.decimal  "price",      precision: 8, scale: 2
   end
-
-  create_table "messages", force: :cascade do |t|
-    t.text     "body"
-    t.integer  "conversation_id"
-    t.integer  "user_id"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-  end
-
-  add_index "messages", ["conversation_id"], name: "index_messages_on_conversation_id"
-  add_index "messages", ["user_id"], name: "index_messages_on_user_id"
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
@@ -60,24 +39,5 @@ ActiveRecord::Schema.define(version: 20150226171502) do
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
   end
-
-  create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-  end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
