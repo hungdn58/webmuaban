24193a536943ce5fe57912105832a8c3bdea7c43
hungdn58 <hungdn_58@vnu.edu.vn>
2015-04-12 18:34:06 +0700

    n commit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-	def gravatar_for(user, size = 30)
-  		image_tag gravatar_image_url(user.email, size: size), title: user.name, class: 'img-rounded'
+	def gravatar_for(user, size = 30, title = user.name)
+  		image_tag gravatar_image_url(user.email, size: size), title: title, class: 'img-rounded'
 	end
 end
