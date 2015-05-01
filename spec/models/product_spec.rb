0a2f20fe327b5293a144ab3dd7789a37115b13c9
Cao Minh Lam <caominhlam30395@gmail.com>
2015-05-01 18:38:08 +0700

    complete product model test

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
  
  describe Product do
-   it "is valid with a title, description, image_url and price" do
+   it "is valid with a title, description, image url and price" do
    	product = Product.new(
    	title: 'Asus',
    	description: 'This is a good laptop',
@@ -18,8 +18,18 @@ require 'spec_helper'
 
    it "is invalid without a description" do 
    	product = Product.new(description: nil)
-   	product.valid?
-   	expect(product.errors[:description]).to include("can't be blank")
+      product.valid?
+      expect(product.errors[:description]).to include("can't be blank")
+   end
+
+   it "is invalid without an image url" do 
+      product = Product.new(image_url: nil)
+      product.should_not be_valid
+   end
+
+   it "is invalid without image's format (.jpg, .png, .jpeg)" do
+      product = Product.new(image_url: 'baitap.pdf')
+      product.should_not be_valid
    end
 
    it "is invalid with a price is not an enumerable value"do 
@@ -40,7 +50,7 @@ require 'spec_helper'
    	expect(product.errors[:price]).to include("must be less than or equal to 1000")
    end
 
-   it "price with a text" do
+   it "is invalid with price is a text" do
       product = Product.new(price: "abc")
       product.should_not be_valid
    end
