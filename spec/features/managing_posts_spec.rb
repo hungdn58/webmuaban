de85f8f22a55c2b25b5727f8c3f925f353ac1461
hungdn58 <hungdn_58@vnu.edu.vn>
2015-05-01 16:08:52 +0700

    Adding testes about login with capybara

--- spec/features/managing_posts_spec.rb
+++ spec/features/managing_posts_spec.rb
@@ -32,4 +32,50 @@ feature 'Managing blog posts' do
       expect(page).to have_content 'Not really Awesome Blog Post'
     end
   end
+
+  context 'as an admin user' do
+    background do
+      email = 'admin@example.com'
+      password = 'password'
+      log_in_admin_user
+    end
+
+    def log_in_admin_user(email = 'admin@example.com', password = 'password')
+      reset_session!
+      visit users_path
+      fill_in 'Email', :with => email
+      fill_in 'Password', :with => password
+      click_button 'Log in'
+    end
+
+    scenario 'Posting a new blog' do
+      visit posts_path
+      click_link 'New Post'
+
+      fill_in 'post_title', :with => 'New Blog Post'
+      fill_in 'post_body', :with => 'This post was made from the Admin Interface'
+      click_button 'Create Post'
+
+      expect(page).to have_content 'This post was made from the Admin Interface'
+    end
+
+    context 'with an existing blog post' do
+      background do
+        @post = Post.create(:title => 'Awesome Blog Post', :body => 'Lorem ipsum dolor sit amet')
+      end
+
+      scenario 'Editing an existing blog' do
+        visit post_path(@post)
+
+        click_link 'Edit'
+
+        fill_in 'Title', :with => 'Not really Awesome Blog Post'
+        click_button 'Update Post'
+
+        expect(page).to have_content 'Not really Awesome Blog Post'
+      end
+    end
+   end
+
+
 end
