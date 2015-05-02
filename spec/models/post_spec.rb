0fded5d7c4f297217b948d3371f83e36cd6dbf1d
ducvn58 <ducvn_58@vnu.edu.vn>
2015-05-02 09:15:27 +0700

    create Post model test

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,22 @@
 require 'spec_helper'
 
 describe Post do
-  pending "add some examples to (or delete) #{__FILE__}"
+   it "is valid with a title, body and image url" do
+   	post = Post.new(
+   	title: 'Laptop',
+   	body: 'Asus')
+   	expect(post).to be_valid
+   end
+
+   it "is invalid without a title" do
+   	post = Post.new(title: nil)
+   	post.valid?
+   	expect(post.errors[:title]).to include("can't be blank")
+   end
+
+   it "is invalid without a body" do 
+   	post = Post.new(body: nil)
+      post.valid?
+      expect(post.errors[:body]).to include("can't be blank")
+   end
 end
